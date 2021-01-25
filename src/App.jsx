1f637d88b7667432d895fff9c6701d617da71e7a
Irfan Khan <officialirfanafzal@gmail.com>
2021-01-25 22:17:25 +0530

    load wasm binary function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { fetchFile } from '@ffmpeg/ffmpeg';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 import './App.css';
 
-function App() {
-  return <div></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { load: false };
+  }
+  ffmpeg = createFFmpeg({ log: true, videoInput: null });
+
+  load = async () => {
+    await this.ffmpeg.load();
+    this.setState({ load: true });
+  };
+  componentDidMount() {
+    this.load();
+  }
+
+  onChangeHandler = (e) => {
+    console.log(e);
+  };
+
+  render() {
+    return this.state.video ? (
+      <div>hi</div>
+    ) : (
+      <div>
+        <input
+          onChange={(e) => {
+            this.onChangeHandler(e);
+          }}
+          type="file"
+          name="video"
+          id="video"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
